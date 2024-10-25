47f4610594139905d65ceed7394c5b76ab6c251c
Miconen <micropizza.mc@gmail.com>
2024-10-25 21:29:40 +0300

    Transfer from using an accordion to lists

--- components/ui/skills.tsx
+++ components/ui/skills.tsx
@@ -7,65 +7,49 @@ import {
 
 export default function Skills() {
   return (
-    <div className="flex flex-col gap-4 [&>span]:text-2xl [&>span]:font-bold">
-    <Accordion type="single" collapsible className="w-full">
-      <AccordionItem value="item-1">
-        <AccordionTrigger>Languages & Frameworks</AccordionTrigger>
-        <AccordionContent>
-          <ul>
-            <li>TypeScript</li>
-            <li>React</li>
-            <li>Angular</li>
-            <li>.NET / C#</li>
-            <li>Go</li>
-            <li>Python</li>
-          </ul>
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-2">
-        <AccordionTrigger>Full-stack & Devops</AccordionTrigger>
-        <AccordionContent>
-          <ul>
-            <li>Database Management (Prisma, EF Core)</li>
-            <li>PostgreSQL</li>
-            <li>Docker (Containerization)</li>
-          </ul>
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-3">
-        <AccordionTrigger>API Integration</AccordionTrigger>
-        <AccordionContent>
-          <ul>
-            <li>RESTful APIs</li>
-            <li>Swagger</li>
-            <li>Google Maps API</li>
-            <li>OpenWeatherMap API</li>
-            <li>MyHelsinki Open API</li>
-          </ul>
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-4">
-        <AccordionTrigger>Authentication & Authorization</AccordionTrigger>
-        <AccordionContent>
-          <ul>
-            <li>User Authentication (Login & Register Functionality)</li>
-            <li>User Authorization with JWTs</li>
-          </ul>
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem value="item-5">
-        <AccordionTrigger>Soft skills</AccordionTrigger>
-        <AccordionContent>
-          <ul>
-            <li>Motivated to learn</li>
-            <li>Problem-solving</li>
-            <li>Communication</li>
-            <li>Teamwork</li>
-            <li>Can-do attitude</li>
-          </ul>
-        </AccordionContent>
-      </AccordionItem>
-    </Accordion>
+    <div className="flex flex-col gap-4 [&>h3]:text-2xl [&>h3]:font-bold [&>ul>ul]:ml-4">
+      <h3>Languages & Frameworks</h3>
+      <ul>
+        <li>JavaScript & TypeScript</li>
+        <ul>
+          <li>Node.js</li>
+          <li>React</li>
+          <li>Angular</li>
+        </ul>
+        <li>.NET / C#</li>
+        <li>Go</li>
+        <li>Python</li>
+      </ul>
+
+      <h3>Full-stack</h3>
+      <ul>
+        <li>Relational databases (PostgreSQL, MySQL, MariaDB)</li>
+        <li>DB Querying</li>
+        <ul>
+          <li>SQL</li>
+          <li>Prisma</li>
+          <li>EF Core</li>
+          <li>Kysely</li>
+        </ul>
+        <li>User authentication and authorization</li>
+        <li>JWTs</li>
+      </ul>
+
+      <h3>???</h3>
+      <ul>
+        <li>Docker (Containerization)</li>
+        <li>CI/CD Pipelines & Devops</li>
+        <li>Strong with UNIX systems</li>
+      </ul>
+
+      <h3>Soft skills</h3>
+      <ul>
+        <li>Motivated to learn</li>
+        <li>Problem-solving</li>
+        <li>Communication</li>
+        <li>Teamwork</li>
+        <li>Can-do attitude</li>
+      </ul>
     </div>
   );
 }
